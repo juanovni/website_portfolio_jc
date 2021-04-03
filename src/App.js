b88f7bb4c05b3b34d2e8b94edc801e9ce90a329d
JuanOvni29 <constantinemurillo@gmail.com>
2021-04-02 23:30:30 -0500

    Se añadio la pagina error 404

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomeContainer from "./pages/HomeContainer";
 import Nav123 from "./components/Nav123";
+import NotFound from "./pages/400";
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={HomeContainer} />
       <Route exact path="/portfolio" component={Nav123} />
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 );
